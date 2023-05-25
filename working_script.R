11c1e97598ae2b745b45e3f29f6d2b45d1d37d4b
nineluijendijk <nineluijendijk@gmail.com>
2023-05-25 17:29:43 +0200

    all plots working except for number count

--- working_script.R
+++ working_script.R
@@ -2,8 +2,11 @@ library(readxl)
 library(tidyverse)
 library(gdata)
 library(car)
+library(cowplot)
+library(magick)
+library(here)
 
-data_raw <- read_excel("data/arabidopsis_data.xlsx", col_types = c("text", rep("numeric", 8), "text")) #making sure every value is numeric
+data_raw <- read_excel(here("data/arabidopsis_data.xlsx"), col_types = c("text", rep("numeric", 8), "text")) #making sure every value is numeric
 
 metadata <- tibble("Group" = c("L1", "L2", "S3", "S4", "L5", "L6", "S7", "S8"),
        "Population" = rep(c("Bovra", "Smadalen", "Helin", "Spiterstulen"), each = 2),
@@ -42,24 +45,23 @@ tidy_sw_results %>% filter(ShapiroWilk_p.value < 0.05) #filter for which groups 
 #All groups of interest are normally distributed, except for the difference in number of leaves
 
 leveneTest(data_complete$Wet_weight ~ as.factor(Treatment), data = data_complete) #Pr(>F) = 0.4168, which means equal variance
-leveneTest(data_complete$Length_dif ~ Size*Treatment, data = data_complete) #Pr(>F) = 0.5969, which means equal variance
+leveneTest(data_complete$Length_dif ~ as.factor(Treatment), data = data_complete) #Pr(>F) = 0.4793, which means equal variance
 
 t.test(formula = data_complete$Wet_weight ~ data_complete$Treatment, 
        paired = FALSE, var.equal = TRUE)$p.value %>% round(.,3) #t-test, p = 0.011, which means there is a statistical significant difference between the salted and unsalted plants.
 
 summary <- data_complete %>%
   group_by(Treatment) %>%
-  summarize(mean_leaves.increase = mean(Number_dif, na.rm=TRUE), stdev=sd(Number_dif, na.rm=TRUE))
-
-
+  summarize(mean_noleaves.increase = mean(Number_dif, na.rm = TRUE),
+            mean_lengthleave.increase = mean(Length_dif, na.rm = TRUE),
+            mean_wetweight = mean(Wet_weight, na.rm = TRUE),
+            stdevNumber = sd(Number_dif, na.rm = TRUE))
 
 data_salted <- subset(data_complete, Treatment == "Salted")
 leveneTest(data_salted$Wet_weight ~ as.factor(Size), data = data_salted) #Pr(>F) = 0.3754 which means equal variance
 
 t.test(formula = data_salted$Wet_weight ~ data_salted$Size, 
        paired = FALSE, var.equal = TRUE)$p.value %>% round(.,3) #t-test, p = 0.848, which means there is no statistical significant difference in wet weight between the large and small salted plants.
-
-
 
 data_salted <- subset(data_complete, Treatment == "Salted")
 leveneTest(data_salted$Length_dif ~ as.factor(Size), data = data_salted) #Pr(>F) = 0.7135 which means equal variance
@@ -68,3 +70,52 @@ t.test(formula = data_salted$Length_dif ~ data_salted$Size,
        paired = FALSE, var.equal = TRUE)$p.value %>% round(.,3) #t-test, p = 0.563, which means there is no statistical significant difference between the difference in length of the longest leaf of the large and small salted plants.
 
 wilcox.test(Number_dif ~ Size, data = data_salted) #Wilcoxon test, p = 0.4735, which means there is no statistical significant difference between the difference in number of leaves of the large and smal salted plants.
+
+summary %>% #plot the increase in number of leaves grouped by treatment
+  ggplot()+
+  geom_col(aes(x = Treatment, y = mean_noleaves.increase, fill = Treatment))+
+  geom_errorbar(aes(ymin = mean_noleaves.increase-stdevNumber, ymax = mean_noleaves.increase+stdevNumber), width=.2)+
+  theme_light()+
+  labs(title = "Increase of number of leaves,\ngrouped by treatment",
+       subtitle = "errorbars depict 1 standard deviation",
+       x="Treatment",
+       y="Mean increase in the number of leaves")+
+  theme(legend.position = "none", text = element_text(size=16))
+
+
+data_color <- data_complete %>% #prepare color data for plotting
+  mutate(Color_graph = case_when(
+    Color == 1 ~ "Green",
+    Color == 2 ~ "Yellow",
+    Color == 3 ~ "Red",
+    Color == 4 ~ "Red and\nyellow"))
+
+plot_color <- data_color %>% #plot number of leaves of each color, grouped by treatment
+  ggplot()+ 
+  geom_bar(aes(x = Color_graph, fill = Treatment), position = position_dodge2(width = 0.9, preserve = "single"))+
+  theme_light()+
+  labs(title = "Number of leaves of each color,\ngrouped by treatment",
+       x="Color",
+       y="Count")+
+
+plot_shape_salt <- data_complete %>% #plot number of leaves of each shape, grouped by treatment
+  ggplot()+ 
+  geom_bar(aes(x = Leaf_shape, fill = Treatment), position = position_dodge2(width = 0.9, preserve = "single"))+
+  theme_light() +
+  labs(title = "Number of leaves of each shape,\ngrouped by treatment",
+       x="Leaf shape",
+       y="Countt")
+
+plot_shape_pop <- data_complete %>% #plot number of leaves of each shape, grouped by population size
+  ggplot()+ 
+  geom_bar(aes(x = Leaf_shape, fill = Size), position = position_dodge2(width = 0.9, preserve = "single"))+
+  theme_light() +
+  labs(title = "Number of leaves of each shape,\ngrouped by population size",
+       x="Leaf shape",
+       y="Countt")
+
+img_leafshapes <- image_read(here("images/leaf_shapes.png")) %>% image_ggplot() #import the picture showing the different leaf shapes
+
+plot_grid(plot_color, img_leafshapes, plot_shape_salt, plot_shape_pop, #combine the 3 plots and the leaf shape photo into 1
+          labels = c("A", "B", "C", "D"),
+          ncol = 2, nrow = 2)
