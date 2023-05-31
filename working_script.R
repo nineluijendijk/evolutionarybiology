985c17a3106ac2206a194cabc424761eecdfef42
nineluijendijk <nineluijendijk@gmail.com>
2023-05-31 18:25:53 +0200

    changed analysis to filter for salted groups rather than the full data

--- working_script.R
+++ working_script.R
@@ -140,7 +140,7 @@ plot_length <- summary_salted %>% #plot the increase in longest leaf length grou
                     ymax = mean_lengthleaves.increase + stdevlengthleave.increase), width=.2)+
   theme_light()+
   labs(title = "Increase in length of the longest\nleaf, grouped by population",
-       subtitle = "Error bars depict 1 standard deviation,\nplot only showing salted plants",
+       subtitle = "Error bars depict 1 standard deviation,\nplot only showing salted groups",
        x="Population size",
        y="Mean increase in the length\nof the longest leaf in cm")+
   theme(legend.position = "none", text = element_text(size=12), plot.title = element_text(size=14),
@@ -154,7 +154,7 @@ plot_noleaves <- summary_salted %>% #plot the increase in number of leaves group
                     ymax = mean_noleaves.increase + stdevNumber), width=.2)+
   theme_light()+
   labs(title = "Increase in number of leaves,\ngrouped by population size",
-       subtitle = "Error bars depict 1 standard deviation,\nplot only showing salted plants",
+       subtitle = "Error bars depict 1 standard deviation,\nplot only showing salted groups",
        x="Population size",
        y="Mean increase in the number of leaves")+
   theme(legend.position = "none", text = element_text(size=12), plot.title = element_text(size=15),
@@ -168,7 +168,7 @@ plot_wetweight <- summary_salted %>% #plot the increase in number of leaves grou
                     ymax = mean_wetweight + stdevwetweight), width=.2)+
   theme_light()+
   labs(title = "Wet weight of the plants,\ngrouped by population size",
-       subtitle = "Error bars depict 1 standard deviation,\nplot only showing salted plants",
+       subtitle = "Error bars depict 1 standard deviation,\nplot only showing salted groups",
        x="Population size",
        y="Mean wet weight in grams")+
   theme(legend.position = "none", text = element_text(size=12), plot.title = element_text(size=15),
@@ -181,29 +181,29 @@ plotgrid1 <- plot_grid(plot_length, plot_wetweight, plot_noleaves, #combine the 
 
 plotgrid1
 
-data_complete %>%
+data_salted %>%
   group_by(Population) %>%
   summarise(ShapiroWilk_p.value = shapiro.test(Length_dif)$p.value) #Shapiro-Wilk test, all p-values are larger than 0.05
 
-data_complete %>%
+data_salted %>%
   group_by(Population) %>%
   summarise(ShapiroWilk_p.value = shapiro.test(Wet_weight)$p.value) #Shapiro-Wilk test, all p-values are larger than 0.05
 
-data_complete %>%
+data_salted %>%
   group_by(Population) %>%
   summarise(ShapiroWilk_p.value = shapiro.test(Number_dif)$p.value) #Shapiro-Wilk test, one of the p-values is lower than 0.05
 
-leveneTest(data_complete$Length_dif ~ as.factor(Population), data = data_complete) #Pr(>F) = 0.5092, which means equal variance
+leveneTest(data_salted$Length_dif ~ as.factor(Population), data = data_salted) #Pr(>F) = 0.8361, which means equal variance
 
-leveneTest(data_complete$Wet_weight ~ as.factor(Population), data = data_complete) #Pr(>F) = 0.5227, which means equal variance
+leveneTest(data_salted$Wet_weight ~ as.factor(Population), data = data_salted) #Pr(>F) = 0.7846, which means equal variance
 
-aov(Length_dif ~ Population, data_complete) %>% summary.aov() #Pr(>F) = 0.0635, which means there is no statistically significant difference
+aov(Length_dif ~ Population, data_salted) %>% summary.aov() #Pr(>F) = 0.478, which means there is no statistically significant difference
 
-aov(Wet_weight ~ Population, data_complete) %>% summary.aov() #Pr(>F) = 0.397, which means there is no statistically significant difference
+aov(Wet_weight ~ Population, data_salted) %>% summary.aov() #Pr(>F) = 0.961, which means there is no statistically significant difference
 
-kruskal.test(Number_dif ~ Population, data = data_complete) #Kruskal-Wallis test, p = 0.0002492, which means there is a statistically significant difference
+kruskal.test(Number_dif ~ Population, data = data_salted) #Kruskal-Wallis test, p = 0.03029, which means there is a statistically significant difference
 
-summary_complete_pop <- data_complete %>%
+summary_complete_pop <- data_salted %>%
   group_by(Population) %>%
   summarize(mean_noleaves.increase = mean(Number_dif, na.rm = TRUE),
             mean_lengthleaves.increase = mean(Length_dif, na.rm = TRUE),
@@ -219,7 +219,7 @@ plot_length2 <- summary_complete_pop %>% #plot the increase in longest leaf leng
                     ymax = mean_lengthleaves.increase + stdevlengthleave.increase), width=.2)+
   theme_light()+
   labs(title = "Increase in length of the longest\nleaf, grouped by population",
-       subtitle = "Error bars depict 1 standard deviation",
+       subtitle = "Error bars depict 1 standard deviation,\nplot only showing salted groups",
        x="Population",
        y="Mean increase in the length\nof the longest leaf in cm")+
   theme(legend.position = "none", text = element_text(size=12), plot.title = element_text(size=14),
@@ -233,7 +233,7 @@ plot_noleaves2 <- summary_complete_pop %>% #plot the increase in number of leave
                     ymax = mean_noleaves.increase + stdevNumber), width=.2)+
   theme_light()+
   labs(title = "Increase in number of leaves,\ngrouped by population",
-       subtitle = "Error bars depict 1 standard deviation",
+       subtitle = "Error bars depict 1 standard deviation,\nplot only showing salted groups",
        x="Population",
        y="Mean increase in the number of leaves")+
   theme(legend.position = "none", text = element_text(size=12), plot.title = element_text(size=15),
@@ -247,7 +247,7 @@ plot_wetweight2 <- summary_complete_pop %>% #plot the increase in number of leav
                     ymax = mean_wetweight + stdevwetweight), width=.2)+
   theme_light()+
   labs(title = "Wet weight of the plants,\ngrouped by population",
-       subtitle = "Error bars depict 1 standard deviation",
+       subtitle = "Error bars depict 1 standard deviation,\nplot only showing salted groups",
        x="Population",
        y="Mean wet weight in grams")+
   theme(legend.position = "none", text = element_text(size=12), plot.title = element_text(size=15),
@@ -281,6 +281,7 @@ plot_color2 <- data_color %>% #plot number of leaves of each color, grouped by p
            position = position_dodge2(width = 0.9, preserve = "single"))+
   theme_light()+
   labs(title = "Number of leaves of each color,\ngrouped by population",
+       subtitle = "Plot only showing salted groups",
        x="Color",
        y="Count")+
   scale_fill_manual(values=c("#8111ee", "#7eee11", "#f0860f", "#0f79f0"))+
@@ -295,22 +296,24 @@ plot_shape_salt <- data_complete %>% #plot number of leaves of each shape, group
        x="Leaf shape",
        y="Count")
 
-plot_shape_popsize <- data_complete %>% #plot number of leaves of each shape, grouped by population size
+plot_shape_popsize <- data_salted %>% #plot number of leaves of each shape, grouped by population size
   ggplot()+ 
   geom_bar(aes(x = Leaf_shape, fill = Size), position = position_dodge2(width = 0.9, preserve = "single"))+
   theme_light() +
   labs(title = "Number of leaves of each shape,\ngrouped by population size",
+       subtitle = "Plot only showing salted groups",
        x="Leaf shape",
        y="Count")+
   scale_fill_manual(values=c("#e457b5", "#57e486"))+
   guides(fill=guide_legend(title="Population size"))
 
-plot_shape_pop <- data_complete %>% #plot number of leaves of each shape, grouped by population
+plot_shape_pop <- data_salted %>% #plot number of leaves of each shape, grouped by population
   ggplot()+ 
   geom_bar(aes(x = Leaf_shape, fill = Population, color = Size), linewidth = 2,
            position = position_dodge2(width = 0.9, preserve = "single"))+
   theme_light() +
   labs(title = "Number of leaves of each shape,\ngrouped by population",
+       subtitle = "Plot only showing salted groups",
        x="Leaf shape",
        y="Count")+
   scale_fill_manual(values=c("#8111ee", "#7eee11", "#f0860f", "#0f79f0"))+
@@ -321,6 +324,6 @@ img_leafshapes <- image_read(here("images/leaf_shapes.png")) %>% image_ggplot() 
 
 plotgrid3 <- plot_grid(plot_color, plot_color2, plot_shape_salt, img_leafshapes, plot_shape_popsize, plot_shape_pop, #combine the 3 plots and the leaf shape photo into 1 figure
           labels = c("A", "B", "C", "D", "E", "F"),
-          ncol =2, nrow = 3)
+          ncol = 2, nrow = 3)
 
 plotgrid3
